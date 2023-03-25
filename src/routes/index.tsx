0250ed5bbb0dbf082286a9e4ca4e00a14bcdfbe9
LucioVitorio <luciovitorio@yahoo.com.br>
2023-03-25 15:26:03 -0300

    feat: adcionado troca de tema

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,16 @@
 import { Button } from "@mui/material";
 import { Navigate, Route, Routes } from "react-router-dom";
+import { useAppThemeContext } from "../shared/contexts/ThemeContext";
 
 export function AppRoutes() {
+  const { toggleTheme } = useAppThemeContext();
+
   return (
     <Routes>
       <Route
         path="/pagina-inicial"
         element={
-          <Button variant="contained" color="primary">
+          <Button variant="contained" color="primary" onClick={toggleTheme}>
             Teste
           </Button>
         }
